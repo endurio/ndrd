d30fecd32769899776cd9b64139cd807a306c2a6
Zergity <Zergity@gmail.com>
2018-10-30 11:45:16 +0700

    Improve mainnet.sh

--- mainnet.sh
+++ mainnet.sh
@@ -9,11 +9,12 @@ remove=0
 daemon=1
 wallet=1
 first=0
+trace=0
 
-while getopts "h?frdw" opt; do
+while getopts "h?frdwt" opt; do
     case "$opt" in
     h|\?)
-        echo "$(basename ""$0"") [-h] [-?] [-f|-r] [-d|-w] nodes_count"
+        echo "$(basename ""$0"") [-h] [-?] [-f|-r] [-d|-w] [t] nodes_count"
         exit 0
         ;;
 	r)	remove=1
@@ -25,6 +26,8 @@ while getopts "h?frdw" opt; do
 		;;
 	w)	daemon=0
 		;;
+	t)	trace=1
+		;;
     esac
 done
 
@@ -32,43 +35,53 @@ shift $((OPTIND-1))
 
 [ "${1:-}" = "--" ] && shift
 
+# OS Compatibility
+PTY=winpty
+START=start
+function killall() {
+	taskkill -F -IM "$1.exe"
+}
+
 CTL="btcctl --rpcuser=a --rpcpass=a --skipverify"
 CTLW="$CTL --wallet"
 
-BTCD="btcd --singlenode --debuglevel=trace --rpcuser=a --rpcpass=a --generate"
+BTCD="btcd --singlenode --rpcuser=a --rpcpass=a --generate"
+if [[ $trace -ne 0 ]]; then
+	BTCD="$BTCD --debuglevel=trace"
+fi
 BTCW="btcwallet --connect=localhost --username=a --password=a"
 
 if [[ $daemon -ne 0 ]]; then
 	if [[ $remove -ne 0 ]]; then
 		rm -rf "$LOCALAPPDATA/btcd/data/mainnet"
 	fi
-	start $BTCD --miningkey=`gpg -d ~/keys/a.gpg`
+	$START $BTCD --miningkey=`gpg -d ~/keys/a.gpg`
 
 	if [[ $first -ne 0 ]]; then
 		sleep 10
 		rm -rf "$LOCALAPPDATA/btcwallet/mainnet"
-		winpty $BTCW --create
-		start $BTCW
+		$PTY $BTCW --create --passphrase=0
+		$START $BTCW
 		sleep 5
 		WALLET_ADDR=`$CTLW getnewaddress`
-		taskkill -F -IM btcwallet.exe
+		killall btcwallet
 
 		rm -rf "$LOCALAPPDATA/btcwalletTMP/mainnet"
-		winpty $BTCW --appdata="$LOCALAPPDATA/btcwalletTMP" --create
-		start $BTCW --appdata="$LOCALAPPDATA/btcwalletTMP"
+		$PTY $BTCW --appdata="$LOCALAPPDATA/btcwalletTMP" --create --passphrase=0
+		$START $BTCW --appdata="$LOCALAPPDATA/btcwalletTMP"
 		sleep 5
 		$CTLW walletpassphrase 0 0
 		$CTLW importprivkey `gpg -d ~/keys/ndr.gpg`
 		$CTLW sendfrom imported $WALLET_ADDR 4 NDR
 		$CTLW importprivkey `gpg -d ~/keys/stb.gpg`
 		$CTLW sendfrom imported $WALLET_ADDR 6 STB
-		taskkill -F -IM btcwallet.exe
+		killall btcwallet
 	fi
 fi
 
 if [[ $wallet -ne 0 ]]; then
 	sleep 2
-	start $BTCW
+	$START $BTCW
 	sleep 5
 	$CTLW walletpassphrase 0 0
 fi
